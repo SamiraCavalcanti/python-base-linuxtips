9c8d8be5f63aa91587285e93f7c8871f010710b9
Samira Cavalcanti <samicavalcanti@gmail.com>
2025-12-30 23:57:36 -0300

    Adiciona tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env  python3
 
+
 """ Imprime a tabuada de 1 ao 10."""
 
 __version__ ="0.1.1"
@@ -17,13 +18,28 @@ for num1 in numeros:
     for num2 in numeros:
         resultado = num1 + num2
         print("{:^20}".format( f"{num1:>2} x {num2:>2} = {resultado:>3}"))
-        print()
+
+print()
+"""Imprime a Tabuada do 1 ao 10."""
+
+__version__ = "0.1.0"
+__autora__ = "Samira Cavalcanti"
+
+#base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros = list(range(1, 11))
+# para cada numero que está em numeros:
+for numero in numeros:
+    print("Tabuada do: ", numero)
+    for outro_numero in numeros:
+        print( numero  * outro_numero)
+    print("------------")
 
 
 
 
 
-    
-           
+
+         
+
 
 
