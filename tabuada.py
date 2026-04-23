4a25180e991727372dfe17aa8d143f378ef50db1
Samira Cavalcanti <samicavalcanti@gmail.com>
2026-04-23 11:49:00 -0300

    Corrigindo tabuada

--- tabuada.py
+++ tabuada.py
@@ -20,19 +20,19 @@ for num1 in numeros:
         print("{:^20}".format( f"{num1:>2} x {num2:>2} = {resultado:>3}"))
 
 print()
-"""Imprime a Tabuada do 1 ao 10."""
 
-__version__ = "0.1.0"
-__autora__ = "Samira Cavalcanti"
 
-#base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-numeros = list(range(1, 11))
-# para cada numero que está em numeros:
-for numero in numeros:
-    print("Tabuada do: ", numero)
-    for outro_numero in numeros:
-        print( numero  * outro_numero)
-    print("------------")
+
+
+
+
+
+
+
+
+
+
+
 
 
 
