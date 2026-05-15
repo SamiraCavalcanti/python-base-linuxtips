674cb27f8c29bf1799952636c345a20e4d47e3b5
Samira Cavalcanti <samicavalcanti@gmail.com>
2026-05-14 21:39:28 -0300

    Refatorando usando dicionários

--- hello.py
+++ hello.py
@@ -17,8 +17,8 @@ Execução:
     ./hello.py
 
 """
-__version__ = "0.0.1"
-__autor__ = "Samira Cavalcanti"
+__version__ = "0.1.2"
+__autora__ = "Samira Cavalcanti"
 __license__ = "Unlicense" 
 
 import os 
@@ -26,19 +26,12 @@ import os
 current_language = os.getenv("LANG", "pt_BR")[:5]
 
 
-msg = "Hello, World!"
-
-if current_language == "pt_BR":
-    msg = "Olá, Mundo !"
-elif current_language == "fr_FR":
-    msg = "Bonjour, le monde !"
-elif current_language == "ko_KR":
-    msg = "안녕하세요, 세상!"
-elif current_language == "es_SP":
-    msg = "Hola, Mundo!"
-elif current_language == "it_IT":
-    msg = "Ciao, Mundo!"
-  
-
-print(msg)
+msg = {
+    "pt_BR": "Olá, Mundo !",
+    "fr_FR": "Bonjour, le monde !",
+    "ko_KR": "안녕하세요, 세상!",
+    "es_SP": "Hola, Mundo!",
+    "it_IT": "Ciao, Mundo!"
+}
+print(msg.get(current_language, "Hello, World!"))
 
